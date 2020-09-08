52e10cc2472ba0bd965010f95ba44f8ce306f506
Dm_Grt <grytsishin@gmail.com>
2020-09-08 11:44:43 +0300

    ShoppingCartService refactored

--- src/main/java/internet/shop/service/ShoppingCartService.java
+++ src/main/java/internet/shop/service/ShoppingCartService.java
@@ -3,8 +3,7 @@ package internet.shop.service;
 import internet.shop.model.Product;
 import internet.shop.model.ShoppingCart;
 
-public interface ShoppingCartService {
-    ShoppingCart create(ShoppingCart shoppingCart);
+public interface ShoppingCartService extends GenericService<ShoppingCart, Long> {
 
     ShoppingCart addProduct(ShoppingCart shoppingCart, Product product);
 
@@ -12,7 +11,4 @@ public interface ShoppingCartService {
 
     void clear(ShoppingCart shoppingCart);
 
-    ShoppingCart getByUserId(Long userId);
-
-    boolean delete(Long id);
 }
--- src/main/java/internet/shop/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/internet/shop/service/impl/ShoppingCartServiceImpl.java
@@ -6,6 +6,7 @@ import internet.shop.lib.Service;
 import internet.shop.model.Product;
 import internet.shop.model.ShoppingCart;
 import internet.shop.service.ShoppingCartService;
+import java.util.List;
 
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
@@ -15,6 +16,16 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Override
     public ShoppingCart create(ShoppingCart shoppingCart) {
         return shoppingCartDao.create(shoppingCart);
+    }
+
+    @Override
+    public List<ShoppingCart> getAll() {
+        return shoppingCartDao.getAll();
+    }
+
+    @Override
+    public ShoppingCart update(ShoppingCart item) {
+        return shoppingCartDao.update(item);
     }
 
     @Override
@@ -38,7 +49,7 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     }
 
     @Override
-    public ShoppingCart getByUserId(Long userId) {
+    public ShoppingCart get(Long userId) {
         return shoppingCartDao.getByUserId(userId).get();
     }
 
