26a13d5cf2b83935bae6f4a4aa41a6e3e39367c2
Dm_Grt <43096165+DmGrt@users.noreply.github.com>
2020-09-15 13:37:15 +0300

    login logic added (#7)
    * login logic added
    
    * login logic added
    
    * user_id replaced with constant, null check added

---
+++ src/main/java/internet/shop/security/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,24 @@
+package internet.shop.security.impl;
+
+import internet.shop.exceptions.AuthenticationException;
+import internet.shop.lib.Inject;
+import internet.shop.lib.Service;
+import internet.shop.model.User;
+import internet.shop.security.AuthenticationService;
+import internet.shop.service.UserService;
+import java.util.Optional;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String login, String password) throws AuthenticationException {
+        Optional<User> userFromDB = userService.findByLogin(login);
+        if (userFromDB.isPresent() && userFromDB.get().getPassword().equals(password)) {
+            return userFromDB.get();
+        }
+        throw new AuthenticationException("Incorrect username or password!");
+    }
+}
--- src/main/java/internet/shop/service/UserService.java
+++ src/main/java/internet/shop/service/UserService.java
@@ -1,6 +1,8 @@
 package internet.shop.service;
 
 import internet.shop.model.User;
+import java.util.Optional;
 
 public interface UserService extends GenericService<User, Long> {
+    Optional<User> findByLogin(String login);
 }
